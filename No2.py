0998580e85680c6928613c1ed7d7d592ae6d69d1
GONNE <gonne411@gmail.com>
2026-03-10 23:18:32 +0200

    added another codeWar

---
+++ No2.py
@@ -0,0 +1,18 @@
+# from statistics import mean
+
+def get_grade(s1, s2, s3):
+#     score = mean([s1, s2, s3]) #using mean is good for readablility and scaling later
+    score = (s1 + s2 + s3) / 3 #1st put it in paranthess to follow the BODMAS rule
+     #always do computation before condition
+    if 90 <= score <= 100:
+        return "A"
+    elif 80 <= score < 90:
+        return "B"
+    elif 70 <= score < 80:
+        return "C"
+    elif 60 <= score < 70:
+        return "D"
+    else:
+        return "F"
+
+print(get_grade(70, 50, 80))    
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CodeWars journey, 9th-3-206
 
--Will be addding documenting my CodeWar solution here
+-Will be  documenting my CodeWar solution here
