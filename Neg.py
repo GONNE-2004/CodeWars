54f24d73c8a5979d39e2fa32ec869e5302c41b08
GONNE <gonne411@gmail.com>
2026-09-03 00:31:13 +0200

    added a Neg.py

---
+++ Neg.py
@@ -0,0 +1,12 @@
+# changing number to negative
+def make_negative(number):
+    if number == 0:
+        return 0
+    elif number < 0:  # numbers below 0 are automatically negative
+        return number
+    else:
+        return -number  # just added the minus to change to neg
+
+
+# i would only say order makes it sensible
+# saw many clever variations in the platform
