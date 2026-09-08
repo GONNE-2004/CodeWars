dce6f998df0a482fcd01979cb882bb208878938c
GONNE <gonne411@gmail.com>
2026-09-08 23:45:06 +0200

    added a codewar example in neg.py

--- Neg.py
+++ Neg.py
@@ -10,3 +10,18 @@ def make_negative(number):
 
 # i would only say order makes it sensible
 # saw many clever variations in the platform
+
+
+# example
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+# def grow(arr):
+#     total = 1
+#     for num in arr:
+#         total *= num
+#     return total
+
+
+# had to set total to 1 so that it could accumulates
+# then loop through each while adding it total
